9ab9b59b9ca09662f5dc0705f3bf486680edd4c7
stephen <isaacsteverado9@gmail.com>
2025-01-30 01:50:14 +0100

    working on data structure

---
+++ declerationOfArray.js
@@ -0,0 +1,19 @@
+//ways to declear an array
+let arrayName = ['value1', 'value2'];//method 1
+
+let array = new Array(); //method 2
+
+//initializing while declearing
+//create an array having elements 10, 20, 30, 40, 50
+const house = new Array(10, 20, 30, 40, 50);
+
+//create an array of 5 undefined elements
+const house1 = new Array(5);
+
+//create an array with element 1BHK
+const home = new Array('1BHK');
+
+console.log(house);
+console.log(house1);
+console.log(home);
+
---
+++ stringImplimentation.js
@@ -0,0 +1,19 @@
+//string written inside quote
+const x = 'Welcome to GeeksforGeeks!';
+console.log(x);
+
+//Declear an object
+const y = new String('Great Geek');
+console.log(y);
+
+//find the first index of the character 'b'
+const a = 'abcdefgh';
+console.log(a.indexOf('b'));
+
+const a2 = 'Hello World!';
+
+const arrString = ['Geeks', 'for', 'Geeks'];
+
+console.log(a2.replace('World', arrString[0]));
+
+
